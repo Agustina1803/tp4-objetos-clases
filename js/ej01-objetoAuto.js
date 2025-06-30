9967cea746676a9ed2f72f94d149b6b52d259f67
AgusBulacio <agusbulacio1803@gmail.com>
2025-06-30 15:25:47 -0300

    agrege el ejercicio 1 del tp4 para que encienda o apague el auto

---
+++ js/ej01-objetoAuto.js
@@ -0,0 +1,23 @@
+const auto={
+    color: 'Rojo',
+    marca: 'Ford',
+    modelo:'Civic',
+    anio: 2021,
+    encendido: false,
+
+encender: function (encendido){
+    let respuesta= prompt(`Desea encender el auto? Si o No`);
+    respuesta = respuesta.toLowerCase();
+    if(respuesta == 'si'){
+        encendido = true;
+        document.writeln(`Auto encendido`);
+    }else{
+        if(respuesta == 'no'){
+           document.writeln(`Auto apagado`);
+        }
+    }
+}
+}
+
+
+auto.encender();
