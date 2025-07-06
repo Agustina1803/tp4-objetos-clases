28c74993a86ffacf2750574e7f290ee7fb6f440f
AgusBulacio <agusbulacio1803@gmail.com>
2025-07-06 02:59:57 -0300

    agrege la logica del ejercicio 3 para que me deje ingresar un alumno y sus notas y me calcule su nota promedio y muestre una tabla

---
+++ js/ej03-objetoAlumno.js
@@ -0,0 +1,37 @@
+const alumnos = [];
+
+do{
+    const nombre = prompt('Nombre del alumno/a: ');
+    const notas = [];
+    do{
+        const nota = parseFloat(prompt(`Ingrese una nota para el alumno/a ${nombre}: `));
+        if(nota >= 1 && nota <= 10 ){
+        notas.push(nota);
+        }else{
+            alert('La nota igresada debe estar entre 1 y 10 ')
+        }
+    }while(confirm(`¿Quiere ingresar mas notas para ${nombre}?`));
+    
+    const alumno = {
+        nombre: nombre,
+        notas: notas
+    };
+    alumnos.push(alumno);
+}while(confirm(`¿Quiere ingresar otro alumno/a? o Presione cancelar para continuar`));
+
+function promedio (notas){
+    let suma =0; 
+  
+    for (let i=0; i< notas.length; i++){
+        suma = suma + notas[i];
+    }
+    return suma / notas.length;
+}
+
+
+document.writeln(`<table class='table table-bordered text-center'><thead><tr><th>Alumno</th><th>Notas</th><th>Promedio</th></tr></thead><tbody>`)
+for (let i = 0; i < alumnos.length; i++) {
+const alumno = alumnos[i];
+  document.writeln(`<tr><td>${alumno.nombre}</td><td>${alumno.notas}</td><td>${promedio(alumno.notas)}</td></tr>`);
+}
+document.writeln(`</tbody></table>`);
