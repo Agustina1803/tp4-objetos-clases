153869856beea14e46cc02ad6f5e4dcd3fc90ae9
AgusBulacio <agusbulacio1803@gmail.com>
2025-07-06 20:40:06 -0300

    cambie de lugar la funcion en el punto 3 de los ejercicios extras

--- js/ej03-objetoAlumno.js
+++ js/ej03-objetoAlumno.js
@@ -1,5 +1,14 @@
 const alumnos = [];
 
+function promedio (notas){
+    let suma =0; 
+  
+    for (let i=0; i< notas.length; i++){
+        suma = suma + notas[i];
+    }
+    return suma / notas.length;
+}
+
 do{
     const nombre = prompt('Nombre del alumno/a: ');
     const notas = [];
@@ -18,16 +27,6 @@ do{
     };
     alumnos.push(alumno);
 }while(confirm(`¿Quiere ingresar otro alumno/a? o Presione cancelar para continuar`));
-
-function promedio (notas){
-    let suma =0; 
-  
-    for (let i=0; i< notas.length; i++){
-        suma = suma + notas[i];
-    }
-    return suma / notas.length;
-}
-
 
 document.writeln(`<table class='table table-bordered text-center'><thead><tr><th>Alumno</th><th>Notas</th><th>Promedio</th></tr></thead><tbody>`)
 for (let i = 0; i < alumnos.length; i++) {
