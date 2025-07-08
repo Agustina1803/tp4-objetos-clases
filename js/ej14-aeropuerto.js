19d85e56b4a64acf4353ea9afb91ec7da33dccad
AgusBulacio <agusbulacio1803@gmail.com>
2025-07-08 19:47:37 -0300

    agrege logica para mostrar por pantalla

--- js/ej14-aeropuerto.js
+++ js/ej14-aeropuerto.js
@@ -35,6 +35,7 @@ class Avion{
         this.#destino = nuevoDestino
     }
 
+
     abordar(pasajero){
         if (this.#listaPasajeros.length < this.capacidad){
             this.#listaPasajeros.push(pasajero);
@@ -84,20 +85,35 @@ class Aeropuerto {
 
 const aeropuerto = new Aeropuerto(`Aeropuerto Internacional`);
 
-const avion1 = new Avion(`Aeropuerto Internacional Ezeiza`, 2, `Madrid`);
-const avion2 = new Avion(`Internacional Jorge Newbery`, 3, `Lima`);
-const avion3 = new Avion(`Internacional de Córdoba`, 1, `Santiago`);
+const nombre1 = prompt(`Ingrese el nombre del primer avion`);
+const capacidad1 =parseInt(prompt(`Ingrese la capacidad del primer  avion`));
+const destino1 = prompt(`Ingrese el destino del primer  avion`);
+
+ const avion1 = new Avion(nombre1, capacidad1, destino1);
+
+ const nombre2 = prompt(`Ingrese el nombre del segundo avion`);
+const capacidad2 =parseInt(prompt(`Ingrese la capacidad del  segundo avion`));
+const destino2 = prompt(`Ingrese el destino del segundo avion`);
+
+ const avion2 = new Avion(nombre2, capacidad2, destino2);
+
+ const nombre3 = prompt(`Ingrese el nombre del tercer avion`);
+const capacidad3 =parseInt(prompt(`Ingrese la capacidad del tercer avion`));
+const destino3 = prompt(`Ingrese el destino del tercer avion`);
+
+const avion3 = new Avion(nombre3, capacidad3, destino3);
 
 aeropuerto.agregarAvion(avion1);
 aeropuerto.agregarAvion(avion2);
 aeropuerto.agregarAvion(avion3);
 
-const avionBuscado = aeropuerto.buscarAvion(`Internacional Jorge Newbery`);
+const nombreABuscar = prompt ('Ingrese el nombre del avion a bucar')
+const avionBuscado = aeropuerto.buscarAvion(nombreABuscar);
 
 
 if (avionBuscado !== null) {
-    avionBuscado.abordar("Agustina");
-    avionBuscado.abordar("Lucas");
-    avionBuscado.abordar("Carla");
-    avionBuscado.abordar("Martina"); 
+    for (let i = 0; i < avionBuscado.capacidad; i++) {
+        const nombreAbordar = prompt(`Ingrese el nombre del pasajero:`);
+        avionBuscado.abordar(nombreAbordar);
+    }
 }
